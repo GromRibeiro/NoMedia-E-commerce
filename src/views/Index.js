261b75d03494a0e447f0a18ecb645f67ef18944d
Gabriel Ribeiro <89033176+GromRibeiro@users.noreply.github.com>
2022-12-16 15:05:58 -0300

    first commit

---
+++ src/views/Index.js
@@ -0,0 +1,256 @@
+/*eslint-disable*/
+import React from "react";
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+export default function Index() {
+  return (
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+                Um E-commerce exatamente igual qualquer outro -
+                NoMedia
+              </h2>
+              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">Projeto focado no aprendizado do
+                React /{" "}
+                <a
+                  href=""
+                  className="text-blueGray-600"
+                >
+                  Node.js
+                </a>
+                   , com intuito de aumento de portifólio.
+              </p>
+              <div className="mt-12">
+                <a
+                  href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
+                  className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                  target="_blank"
+                >
+                  Github Star
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px"
+          src={require("assets/img/pattern_react.png").default}
+          alt="..."
+        />
+      </section>
+
+      <section className="mt-48 md:mt-40 pb-40 relative bg-blueGray-100">
+        <div
+          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-blueGray-100 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+        <div className="container mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
+                <img
+                  alt="..."
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+                  className="w-full align-middle rounded-t-lg"
+                />
+                <blockquote className="relative p-8 mb-4">
+                  <svg
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 583 95"
+                    className="absolute left-0 w-full block h-95-px -top-94-px"
+                  >
+                    <polygon
+                      points="-30,95 583,95 583,65"
+                      className="text-lightBlue-500 fill-current"
+                    ></polygon>
+                  </svg>
+                  <h4 className="text-xl font-bold text-white">
+                    Projeto SemMedia um E-commerce igual qualquer outro
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    Simplesmente um E-commerce igual a Amazon , Mercado livre , Americanas
+                    ou até mesmo o E-bay. O nosso foco é copiar mesmo porém um tiquin melhor.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 px-4">
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        CSS Components
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Componentes CSS com Tailwind pra fica dqnipao a produção disso aqui
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        JavaScript Components
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        We also feature many dynamic components for React,
+                        NextJS, Vue and Angular
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Pages</h6>
+                      <p className="mb-4 text-blueGray-500">
+                       São varias páginas no projeto SemMedia contando com até mesmo
+                       uma tela dashboard para o controle do admin do site!
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-file-alt"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Documentation
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Built by developers for developers. You will love how
+                        easy is to to work with Notus React.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blueGray-600 overflow-hidden">
+        <div className="container mx-auto pb-64">
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full md:w-5/12 px-12 md:px-4 ml-auto mr-auto md:mt-64">
+              <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                <i className="fas fa-code-branch text-xl"></i>
+              </div>
+              <h3 className="text-3xl mb-2 font-semibold leading-normal text-white">
+                Open Source
+              </h3>
+              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-400">
+                Como{" "}
+                <a
+                  href="https://tailwindcss.com/?ref=creativetim"
+                  className="text-blueGray-300"
+                  target="_blank"
+                >
+                  Tailwind CSS
+                </a>{" "}
+                é um projeto de código aberto, queria continuar esse movimento também.
+                Você pode experimentar esta versão para sentir o design e também testar a qualidade do código!
+              </p>
+              <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-400">
+                Obtenha-o gratuitamente no Github e me ajude com uma estrela!
+              </p>
+              <a
+                href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
+                target="_blank"
+                className="github-star mt-4 inline-block text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg"
+              >
+                Github Star
+              </a>
+            </div>
+
+            <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-32 relative">
+              <i className="fab fa-github text-blueGray-700 absolute -top-150-px -right-100 left-auto opacity-80 text-55"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pb-16 bg-blueGray-200 relative pt-32">
+        <div
+          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-blueGray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+
+        <div className="container mx-auto">
+          <div className="flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-16 px-12 relative z-10">
+            <div className="w-full text-center lg:w-8/12">
+              <h3 className="font-semibold text-3xl">
+                Me da uma estrelinha?
+              </h3>
+              <p className="text-blueGray-500 text-lg leading-relaxed mt-4 mb-4">
+                Caso você queira me deixar feliz clique no botão
+                abaixo e deixe uma estrelinha em meu repositório
+                no GitHub!
+              </p>
+              <div className="sm:block flex flex-col mt-10">
+                <a
+                  href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
+                  target="_blank"
+                  className="github-star sm:ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg"
+                >
+                  <i className="fab fa-github text-lg mr-1"></i>
+                  <span>Star</span>
+                </a>
+              </div>
+              <div className="text-center mt-16"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
